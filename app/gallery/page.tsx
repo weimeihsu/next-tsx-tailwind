ecd489f6e61e8bb679ec89fbc1acac6c5508288f
weimeihsu <weimeihsu@users.noreply.github.com>
2024-03-13 15:29:34 +0800

    modify config

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,9 +1,10 @@
 'use client'
+import Image from 'next/image'
 import { useState, useEffect } from "react"
 import { db } from "../firebase/config" 
 import { collection, getDocs } from "firebase/firestore"
 
-export const fetchImg = async () =>{
+const fetchImg = async () =>{
     const querySnapshot = await getDocs(collection(db, 'img-the-crew'))
     const imgs = []
     querySnapshot.forEach(doc=>{
@@ -26,8 +27,8 @@ const Gallery = () => {
         <h1 className="text-xl my-4">Images Fetched from Firebase</h1>
         <div className="grid grid-cols-2 gap-2 md:grid-cols-4">
             {imgs.map(item=>(
-                <div className="p-4 rounded shadow-deeper ">
-                    <img src={item.url} alt={item.title} key={item.id} className="rounded-sm mb-2"/>
+                <div className="p-4 rounded shadow-deeper" key={item.id} >
+                    <Image src={item.url} alt={item.title} className="rounded-sm mb-2"/>
                     <p className="text-sm font-light">{item.title}</p>
                 </div>
             ))}
