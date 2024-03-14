3a0f01665437af59679fccf9af2e101295fd9877
weimeihsu <weimeihsu@users.noreply.github.com>
2024-03-14 15:11:41 +0800

    config output

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
-    // output: 'export',
+    output: 'export',
     images: {
         unoptimized: true,
         remotePatterns: [
