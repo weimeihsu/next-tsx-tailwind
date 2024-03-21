a5b7ddd16b33d415ca8153ce0c9e3ca9f1d5e687
weimeihsu <weimeihsu@users.noreply.github.com>
2024-03-21 12:02:31 +0800

    type folder

---
+++ components/CustomButton.tsx
@@ -0,0 +1,17 @@
+'use client'
+const CustomButton = () => {
+    return ( 
+        <button
+            disabled={false}
+            type={"button"}
+            className={`custom-btn`}
+            onclick={()=>{}}
+        >
+            <span className={`flex-1`}>
+                title
+            </span>
+        </button>
+     );
+}
+ 
+export default CustomButton;
