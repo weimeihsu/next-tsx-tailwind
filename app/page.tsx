7f4e7b03e8c5247a388860623f5236be917dfe30
weimeihsu <weimeihsu@users.noreply.github.com>
2024-03-26 11:57:55 +0800

    cheer up text color to white

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ const Home = () => {
     <main>
       <div className="p-6 max-w-sm mx-auto flex flex-col items-center space-x-4">
       <div className="flex flex-row gap-x-1">
-        <FerrisWheel className="text-steel-blue"/>
-        <p className="text-xl text-steel-blue">Cheer Up!</p>
+        <FerrisWheel className="text-java"/>
+        <p className="text-xl text-java ml-2">Cheer Up!</p>
       </div>
           <Lottie animationData={anima}/>
       </div>
