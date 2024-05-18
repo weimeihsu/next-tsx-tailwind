8d08cb01622a7185fc152f12b7a7a77a0d62404b
weimeihsu <weimei0928@gmail.com>
2024-05-19 00:35:21 +0800

    install storybook

---
+++ components/Stack.tsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types'
+
+const Stack = ({ children, spacing = 2, direction = "row", wrap = false }) => {
+    const customStyle = {
+        display: "flex",
+        gap: `${spacing * 0.25}rem`,
+        flexWrap: wrap ? "wrap" : "nowrap",
+        flexDirection: direction,
+      }
+
+    return ( 
+        <div style={customStyle}>{children}</div>
+     );
+}
+
+Stack.propTypes = {
+    spacing: PropTypes.number,
+    wrap: PropTypes.bool,
+    direction: PropTypes.oneOf(["row", "column"]),
+  }
+
+export default Stack;
